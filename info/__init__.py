0111bb66e6873f48e43dcb8ff0ed51d8b83e7a84
python <17688795539@163.com>
2020-02-27 17:53:12 +0800

    分化管理,工厂模式

---
+++ info/__init__.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+from config import config_dict
+from flask_wtf.csrf import CSRFProtect
+# session拓展工具,　将flask中的session存储调整到redis
+from flask_session import Session
+
+import pymysql
+
+# python2和python３数据库相互转化使用
+pymysql.install_as_MySQLdb()
+
+# 全局变量，申明为空类型数据
+redis_store = None
+
+# 当app不存的时候，只有进行申明，并没有真正创建数据库对象db
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    """
+    工厂方法:development --->DevelopmentConfig/ production --->ProductionConfig
+    """
+    # 1.创建app对象
+    app = Flask(__name__)
+
+    # 2.将配置信息添加到app上
+    config_class = config_dict[config_name]
+
+    # DevelopmentConfig --->赋予app属性：开发模式app
+    # ProductionConfig --->赋予app属性：线上模式app
+    app.config.from_object(config_class)
+
+    # 3.数据库对象(mysal/redis)
+    # mysql数据库对象
+    # db = SQLAlchemy(app)
+    # 延迟加载数据库,当app有值的时候我才真正进行数据库初始化工作
+    db.init_app(app)
+
+    # redis数据库对象
+    global redis_store
+    redis_store = StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT, decode_responses=True)
+
+    # 4.给项目添加CSRF保护机制
+    # 4.1提取cooking中的csrf_token
+    # 4.2如果数据是通过表单发送：提取表单中的csrf_token,如果数据是通过ajax请求发送：提取请求头中的字段X-CSRFToken
+    # 4.3对比这两个值是否一致
+    CSRFProtect(app)
+
+    # 5.创建Flask_session工具对象：将flask.session的存储从　服务器“内存”　调整到　“redis”数据库
+    Session(app)
+
+    # 返回app对象
+    return app
--- manage.py
+++ manage.py
@@ -1,71 +1,13 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from redis import StrictRedis
-from config import config_dict
-from flask_wtf.csrf import CSRFProtect
-# session拓展工具,　将flask中的session存储调整到redis
-from flask_session import Session
 # 将存储数据到的session
 from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
-import pymysql
-
-# python2和python３数据库相互转化使用
-pymysql.install_as_MySQLdb()
+from info import create_app, db
 
 
-# # 0.创建项目配置
-# class Config(object):
-#     """自定义项目配置"""
-#
-#     # 　开启项目调试模式
-#     DEBUG = True
-#
-#     # 　mysql数据库配置
-#     # 连接mysql的配置
-#     SQLALCHEMY_DATABASE_URI = "mysql://root:python@127.0.0.1:3306/news"
-#     # 开启数据库跟踪修改操作
-#     SQLALCHEMY_TRACK_MODIFICATIONS = True
-#
-#     # redis数据库的配置信息
-#     REDIS_HOST = "127.0.0.1"
-#     REDIS_PORT = 6379
-#
-#     # 使用session需要设置加密字符串
-#     SECRET_KEY = "SNFKALFAGDASDFASDGHJ"
-#
-#     # 将flask.session的存储从　服务器“内存”　调整到　“redis”数据库配置如下
-#     SESSION_TYPE = 'redis'  # 标明存储的数据库类型
-#     SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=1)  # 只需要redis对象
-#     SESSION_USE_SIGNER = True  # 对于session_id需要加密处理
-#     SESSION_PERMANENT = False  # redis中的session数据不需要永久存储
-#     PERMANENT_SESSION_LIFTTIME = 86400  # 设置redis中session过期时长
-
-
-# 1.创建app对象
-app = Flask(__name__)
-
-# 2.将配置信息添加到app上
-config_class = config_dict['development']
-app.config.from_object(config_class)
-
-# 3.数据库对象(mysal/redis)
-# mysql数据库对象
-db = SQLAlchemy(app)
-
-# redis数据库对象
-redis_store = StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT, decode_responses=True)
-
-# 4.给项目添加CSRF保护机制
-# 4.1提取cooking中的csrf_token
-# 4.2如果数据是通过表单发送：提取表单中的csrf_token,如果数据是通过ajax请求发送：提取请求头中的字段X-CSRFToken
-# 4.3对比这两个值是否一致
-CSRFProtect(app)
-
-# 5.创建Flask_session工具对象：将flask.session的存储从　服务器“内存”　调整到　“redis”数据库
-Session(app)
+# 传入的参数是development获取开发模式对应的app对象
+# 传入的参数是production获取线上模式对应的app对象
+app = create_app("developmengt")
 
 # 6.创建管理对象
 manager = Manager(app)
